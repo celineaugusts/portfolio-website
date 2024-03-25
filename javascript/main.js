2c45c88cb8944595d8ed173e879ad31af3e370ec
Celine August <celineaugusts@gmail.com>
2024-03-25 05:31:19 -0700

    Added Style Guide
    Finished Style Guides

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Get the height of the navigation bar
-    const navHeight = document.querySelector('header').offsetHeight;
+    const navHeight = document.querySelector('.nav-container').offsetHeight;
     // Set the top margin of the body to be equal to the height of the navigation bar
     document.body.style.marginTop = `${navHeight}px`;
-    document.documentElement.style.setProperty('--scroll-padding', navigationHeight - 1 + "px");
+    document.documentElement.style.setProperty('--scroll-padding', navHeight - 1 + "px");
 });
